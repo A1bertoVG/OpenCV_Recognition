060e566da881d52c703dbc55ddbffdedb04e3e62
A1bertoVG <82616218+A1bertoVG@users.noreply.github.com>
2021-08-12 06:33:07 -0500

    Gray scale pic

--- Recog_cv2.py
+++ Recog_cv2.py
@@ -1,9 +1,17 @@
 import cv2
 from random import randrange
 
+trained_face_data = cv2.CascadeClassifier('haarcascade_frontaface_defailt.xml')
+
 img = cv2.imread('src/media/faces.jpg')
 
-cv2.imshow('Recognition', img)
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+'''face_coord = trained_face_data.detectMultiScale(gray_img)
+
+print(face_coord)'''
+
+cv2.imshow('Recognition', gray_img)
 cv2.waitKey()
 
 print('alles gut')
