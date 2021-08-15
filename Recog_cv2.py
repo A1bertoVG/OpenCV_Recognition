45ea032ff128c5e00df87447391d9239787f1b20
A1bertoVG <82616218+A1bertoVG@users.noreply.github.com>
2021-08-15 05:47:08 -0500

    Car w/ photo recognition

--- Recog_cv2.py
+++ Recog_cv2.py
@@ -1,9 +1,21 @@
 import cv2
 from random import randrange
 
-img = cv2.imread('src/media/faces.jpg')
+img = cv2.imread('src/media/car_img.jpg')
 
-cv2.imshow('Recognition', img)
-cv2.waitKey()
+trained_car_data = cv2.CascadeClassifier('car.xml')  # https://gist.github.com/199995/37e1e0af2bf8965e8058a9dfa3285bc6
+
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+car_coord = trained_car_data.detectMultiScale(gray_img, scaleFactor=1.0949)
+# print(car_coord)
+
+for (x, y, w, h) in car_coord:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+cv2.imshow('Car recognition', img)
+key = cv2.waitKey()
+if key == 81 or key == 113:
+    cv2.destroyAllWindows()
 
 print('alles gut')
