aa0cd99f09e0068f1af64a50011e7af914674e22
A1bertoVG <82616218+A1bertoVG@users.noreply.github.com>
2021-08-15 04:37:33 -0500

    Image recognition done

--- Recog_cv2.py
+++ Recog_cv2.py
@@ -1,17 +1,22 @@
 import cv2
 from random import randrange
 
-trained_face_data = cv2.CascadeClassifier('haarcascade_frontaface_defailt.xml')
+trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 img = cv2.imread('src/media/faces.jpg')
 
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-'''face_coord = trained_face_data.detectMultiScale(gray_img)
+face_coord = trained_face_data.detectMultiScale(gray_img)
 
-print(face_coord)'''
+for (x, y, w, h) in face_coord:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-cv2.imshow('Recognition', gray_img)
+# (x, y, w, h) = face_coord[5] # thisis to make a rectangle in individuel way, between the brackets can write from 0 to 11
+# cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+
+cv2.imshow('Face Recognition App', img)
 cv2.waitKey()
 
 print('alles gut')
