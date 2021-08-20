280fb15b4144753e817f214df009f71850b39f95
A1bertoVG <82616218+A1bertoVG@users.noreply.github.com>
2021-08-20 04:55:33 -0500

    Car recognition from video

---
+++ Realtime_recog.py
@@ -0,0 +1,21 @@
+import cv2
+
+video = cv2.VideoCapture('src/media/car.mp4')
+
+trained_car_data = cv2.CascadeClassifier('car.xml')
+
+while True:
+    succes_video, frame = video.read()
+
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    car_coord = trained_car_data.detectMultiScale(gray_frame)
+
+    for (x,y,w,h) in car_coord:
+        cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0),2)
+
+    cv2.imshow('Real Time Recognition', frame)
+    cv2.waitKey(1)
+
+
+    print('alles gut')
